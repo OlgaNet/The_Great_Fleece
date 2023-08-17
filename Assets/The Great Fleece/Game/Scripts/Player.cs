fd211c6512b6b38189e65f41bd49cff67c05767f
Olga <37110188+OlgaNet@users.noreply.github.com>
2023-08-17 12:59:34 +0100

    Section26: 261. Challenge Review: Point & Click
    Section26: Play Movement
    261. Challenge Review: Point & Click

---
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            //cast a ray from mouse position
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                //debug the floor position hit
+                Debug.Log("Hit:" + hitInfo.point);
+
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = hitInfo.point;
+            }
+        }
+                
+        //create object at floor position
+    }
+}
