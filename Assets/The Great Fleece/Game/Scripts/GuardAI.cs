e6edf8ddfbd0b1b9cd04d3ead7830bcbe5001434
Olga <37110188+OlgaNet@users.noreply.github.com>
2023-08-28 14:43:32 +0100

    !!bugFixed Section27: 289. Challenge Review: Reverse Enemy AI
    Section27: Enemy AI
    289. Challenge Review: Reverse Enemy AI
    
    //error:"ArgumentOutOfRangeException: Index was out of range. Must be non-negative and less than the size of the collection. Parameter name: index"
    
    // Check if there are any waypoints before setting the destination //fixed

--- Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -1,50 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class GuardAI : MonoBehaviour
 {
     public List<Transform> wayPoints;
-    public Transform currentTarget;
-
     private NavMeshAgent _agent;
+    [SerializeField]
+    private int currentTarget;
+    private bool reverse;
 
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-
+        //_agent.SetDestination(wayPoints[currentTarget].position); //error:"ArgumentOutOfRangeException: Index was out of range. Must be non-negative and less than the size of the collection. Parameter name: index"
+        // Check if there are any waypoints before setting the destination //fixed
         if (wayPoints.Count > 0)
         {
-            if (wayPoints[0] != null)
-            {
-                currentTarget = wayPoints[0];
-                _agent.SetDestination(currentTarget.position);
-            }
+            _agent.SetDestination(wayPoints[currentTarget].position);
         }
-        
+        else
+        {
+            Debug.LogWarning("No waypoints assigned for GuardAI!");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentTarget != null)
+        if (wayPoints.Count > 0 && wayPoints[currentTarget] != null)
         {
-            float distance = Vector3.Distance(transform.position, currentTarget.position);
+            _agent.SetDestination(wayPoints[currentTarget].position);
+
+            float distance = Vector3.Distance(transform.position, wayPoints[currentTarget].position);
 
             if (distance < 1.0f)
             {
-                if (wayPoints[1] != null && currentTarget != wayPoints[1])
+                if (reverse == true)
                 {
-                    currentTarget = wayPoints[1];
-                    _agent.SetDestination(currentTarget.position);
+                    currentTarget--;
+                    if (currentTarget == 0)
+                    {
+                        reverse = false;
+                        currentTarget = 0;
+                    }
                 }
-                else if (wayPoints[2] != null)
+                else
                 {
-                    currentTarget = wayPoints[2];
-                    _agent.SetDestination(currentTarget.position);
+                    currentTarget++;
+
+                    if (currentTarget == wayPoints.Count)
+                    {
+                        //you've made it to the end
+                        reverse = true;
+                        currentTarget--;
+                    }
                 }
+
             }
         }
 
