06466c132fb6ca40649c25422b82a8acf67f1a3d
Olga <37110188+OlgaNet@users.noreply.github.com>
2023-08-31 21:29:56 +0100

    Section27: 297. Setup Other Guards
    Section27: Enemy AI
    297. Setup Other Guards

--- Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -10,19 +10,21 @@ public class GuardAI : MonoBehaviour
     public List<Transform> wayPoints;
     private NavMeshAgent _agent;
     [SerializeField]
-    private int currentTarget;
-    private bool reverse;
+    private int _currentTarget;
+    private bool _reverse;
     private bool _targetReached;
+    private Animator _anim;
 
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-        //_agent.SetDestination(wayPoints[currentTarget].position); //error:"ArgumentOutOfRangeException: Index was out of range. Must be non-negative and less than the size of the collection. Parameter name: index"
+        _anim = GetComponent<Animator>();
+        //_agent.SetDestination(wayPoints[_currentTarget].position); //error:"ArgumentOutOfRangeException: Index was out of range. Must be non-negative and less than the size of the collection. Parameter name: index"
         // Check if there are any waypoints before setting the destination //fixed
         if (wayPoints.Count > 0)
         {
-            _agent.SetDestination(wayPoints[currentTarget].position);
+            _agent.SetDestination(wayPoints[_currentTarget].position);
         }
         else
         {
@@ -34,19 +36,51 @@ public class GuardAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (wayPoints.Count > 0 && wayPoints[currentTarget] != null)
+        if (wayPoints.Count > 0 && wayPoints[_currentTarget] != null)
         {
-            _agent.SetDestination(wayPoints[currentTarget].position);
+            _agent.SetDestination(wayPoints[_currentTarget].position);
+            
 
-            float distance = Vector3.Distance(transform.position, wayPoints[currentTarget].position);
+            float distance = Vector3.Distance(transform.position, wayPoints[_currentTarget].position);
+
+            if (distance < 1 && (_currentTarget == 0 || _currentTarget == wayPoints.Count - 1))
+            {
+                _anim.SetBool("Walk", false);
+            }
+            else
+            {
+                _anim.SetBool("Walk", true);
+            }
 
             if (distance < 1.0f && _targetReached == false)
             {
-                _targetReached = true;
-                Debug.Log("Target Reached: " + _targetReached);
+                if (wayPoints.Count < 2)
+                {
+                    return;
+                }
 
-                StartCoroutine(WaitBeforeMoving());
-
+                if ((_currentTarget == 0 || _currentTarget == wayPoints.Count - 1) && wayPoints.Count > 1)
+                {
+                    _targetReached = true;
+                    Debug.Log("Target Reached: " + _targetReached);
+                    StartCoroutine(WaitBeforeMoving());
+                }
+                else
+                {
+                    if (_reverse == true)
+                    {
+                        _currentTarget--;
+                        if (_currentTarget <= 0)
+                        {
+                            _reverse = false;
+                            _currentTarget = 0;
+                        }
+                    }
+                    else
+                    {
+                        _currentTarget++;
+                    }
+                }
             }
         }
 
@@ -54,27 +88,37 @@ public class GuardAI : MonoBehaviour
     IEnumerator WaitBeforeMoving()
     {
         Debug.Log("WaitBeforeMoving()");
-        yield return new WaitForSeconds(2.0f);
 
-        if (reverse == true)
+        if (_currentTarget == 0)
         {
-            currentTarget--;
+            //at beginning
+            yield return new WaitForSeconds(2.0f);
+        }
+        else if (_currentTarget == wayPoints.Count - 1)
+        {
+            //at end
+            yield return new WaitForSeconds(2.0f);
+        }
 
-            if (currentTarget == 0)
+        if (_reverse == true)
+        {
+            _currentTarget--;
+
+            if (_currentTarget == 0)
             {
-                reverse = false;
-                currentTarget = 0;
+                _reverse = false;
+                _currentTarget = 0;
             }
         }
-        else if (reverse == false)
+        else if (_reverse == false)
         {
-            currentTarget++;
+            _currentTarget++;
 
-            if (currentTarget == wayPoints.Count)
+            if (_currentTarget == wayPoints.Count)
             {
                 //you've made it to the end
-                reverse = true;
-                currentTarget--;
+                _reverse = true;
+                _currentTarget--;
             }
         }
 
