79879a8a5e946ae02528bcf0148d16a849468735
Olga <37110188+OlgaNet@users.noreply.github.com>
2023-09-02 10:32:20 +0100

    Finished Section28: Coin Distraction
    Finished Section28: Coin Distraction

---
+++ Assets/The Great Fleece/Game/Scripts/VoiceOverTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceOverTrigger : MonoBehaviour
+{
+    public AudioClip clipToPlay;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            AudioSource.PlayClipAtPoint(clipToPlay, Camera.main.transform.position);
+        }
+    }
+}
