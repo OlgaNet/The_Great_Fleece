8389469d7e882a8b593233439179f2808969533b
Olga <37110188+OlgaNet@users.noreply.github.com>
2023-09-02 13:58:05 +0100

    Finished Section29: Security Cameras
    Finished Section29: Security Cameras

--- Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
+++ Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class SecurityCamera : MonoBehaviour
 {
     public GameObject gameOverCutscene;
+    public Animator anim;     //get a handle to the animator component
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            gameOverCutscene.SetActive(true);
+            //change the material
+            MeshRenderer render = GetComponent<MeshRenderer>();
+            Color color = new Color(0.6f, .1f, .11f, .04f);
+            render.material.SetColor("_TintColor", color);
+            anim.enabled = false;
+
+            StartCoroutine(AlertRoutine());
         }
+    }
+    IEnumerator AlertRoutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        gameOverCutscene.SetActive(true); //call after o.5f
     }
 }
